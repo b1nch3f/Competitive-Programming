70c80ea3a3c08649eef9168d86b6f106b5f3ee47
Sangram Kesari Ray <shankar.ray030@gmail.com>
2021-01-31 07:52:00 +0100

    Initial Commit

---
+++ cp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cp
+//
+//  Created by Sangram Ray on 31.01.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
