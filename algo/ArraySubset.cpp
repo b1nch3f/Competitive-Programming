aa0a102f10be2eb30e8c765d09d8ea875a361e50
Sangram Kesari Ray <shankar.ray030@gmail.com>
2021-02-04 10:13:34 +0100

    refactored

--- algo/ArraySubset.cpp
+++ algo/ArraySubset.cpp
@@ -6,30 +6,40 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 
-void search(vi arr, vi sub, int index) {
-    cout << "( ";
-    
-    for (int item: sub)
-        cout << item << " ";
-    
-    cout << ")" << "\n";
-    
-    for(int i = index; i < arr.size(); i++) {
-        sub.push_back(arr[i]);
-        search(arr, sub, i + 1);
-        sub.pop_back();
+void search(vi arr, int n, int index, vi& subset)
+{
+    if (index == n)
+    {
+        // process subset
+        cout << "( ";
+        for (int item : subset)
+        {
+            cout << item << " ";
+        }
+        cout << ")" << "\n";
+    }
+    else
+    {
+        // include k in the subset
+        subset.push_back(arr[index]);
+        search(arr, n, index+1, subset);
+        subset.pop_back();
+        // don't include k in the subset
+        search(arr, n, index+1, subset);
     }
 }
 
-void solve() {
-    vi arr = {7, 2, 5, 4};
-    vi sub;
+void solve()
+{
+    vi arr = {1};
+    int n = (int)arr.size();
     int index = 0;
-    
-    search(arr, sub, index);
+    vi subset;
+    search(arr, n, index, subset);
 }
 
-int main() {
+int main()
+{
     #ifndef ONLINE_JUDGE
     freopen("in.txt", "r", stdin);
     #endif
--- algo/SubsetSum.cpp
+++ algo/SubsetSum.cpp
@@ -6,39 +6,47 @@ typedef long long ll;
 typedef unsigned long long ull;
 typedef vector<int> vi;
 
-void search(vi arr, vi sub, int index) {
-    cout << "( ";
-    int sum = 0;
-    
-    for (int item: sub) {
-        cout << item << " ";
-        sum += item;
+void search(vi arr, int n, int index, vi& subset)
+{
+    if (index == n)
+    {
+        // process subset
+        cout << "( ";
+        int count = 0;
+        for (int item : subset)
+        {
+            cout << item << " ";
+            count += item;
+        }
+        cout << ")";
+        if (count == 1)
+        {
+            cout << "--";
+        }
+        cout << "\n";
     }
-    
-    cout << ")";
-    
-    if (sum == 9) {
-        cout << " --";
-    }
-    
-    cout << "\n";
-    
-    for(int i = index; i < arr.size(); i++) {
-        sub.push_back(arr[i]);
-        search(arr, sub, i + 1);
-        sub.pop_back();
+    else
+    {
+        // include k in the subset
+        subset.push_back(arr[index]);
+        search(arr, n, index+1, subset);
+        subset.pop_back();
+        // don't include k in the subset
+        search(arr, n, index+1, subset);
     }
 }
 
-void solve() {
-    vi arr = {7, 2, 5, 4};
-    vi sub;
+void solve()
+{
+    vi arr = {1};
+    int n = (int)arr.size();
     int index = 0;
-    
-    search(arr, sub, index);
+    vi subset;
+    search(arr, n, index, subset);
 }
 
-int main() {
+int main()
+{
     #ifndef ONLINE_JUDGE
     freopen("in.txt", "r", stdin);
     #endif
